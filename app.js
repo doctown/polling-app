fc3f3becbf794933abee3de396afd4f1dc9895e9
David Ogor <doctown@yahoo.com>
2016-06-29 12:31:57 -0700

    Initial setup of server

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+app.use(express.static('./public'));
+app.use(express.static('./node_modules/boostrap/dist'));
+
+app.listen(PORT, function() {
+  console.log('Server is listening on port: ', PORT);
+});
