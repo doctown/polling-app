20f2284c9b4c9b5350525b77a84098b31548842d
David Ogor <doctown@yahoo.com>
2016-07-04 18:24:07 -0700

    Update webpack for using react

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 module.exports = {
   entry: './app-client.js',
+  test: /\.jsx?$/,         // Match both .js and .jsx files
   output: {
     filename: "public/bundle.js"
   },
   module: {
     loaders: [
       {
-        exclude: /(node_modules|app-server.js)/,
-        loader: 'babel'
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
       }
     ]
   }
