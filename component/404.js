07e1540f933f30215c764dd611fdec97d6739794
David Ogor <doctown@yahoo.com>
2016-07-31 11:24:11 -0700

    404 response component

--- component/404.js
+++ component/404.js
@@ -1,3 +1,19 @@
-/**
- * Created by docto on 7/31/2016.
- */
+import { React, Link } from 'react';
+
+var Error404 = React.createClass({
+  render() {
+    return (
+      <div id="not-found">
+        <h1>404 Error</h1>
+        <p> You have encountered a page that does not exist!
+          Were you looking for one of these:
+          <Link to="/">Join as Audience</Link>
+          <Link to="/speaker">Start the presentation</Link>
+          <Link to="/board">View the board</Link>
+        </p>
+      </div>
+  );
+  }
+});
+
+module.exports = Error;
